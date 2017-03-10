24bd747da796c664631cc090b82bbf1cefc2755b
Студент <>
2017-03-10 09:28:00 +0300

    третий коммит

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a,b;
+    cout << "Vvedite dva chisla";  cin >> a >>  b;
+    cout << "Bili vvedeni dva chisla "<< a << b << endl;
     return 0;
 }
